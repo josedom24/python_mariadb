d15da14c4cdf7af901a9ffa63502b012434dd404
José Domingo Muñoz <josedom24@gmail.com>
2022-02-08 18:19:49 +0100

    python

---
+++ funciones.py
@@ -0,0 +1,14 @@
+import sys
+import MySQLdb
+
+def Conectar_BD(host,usuario,password,nombrebd):
+    try:
+    	db = MySQLdb.connect(host,usuario,password,nombrebd)
+        return db
+    except MySQLdb.Error as e:
+    	print("No puedo conectar a la base de datos:",e)
+    	sys.exit(1)
+
+def Desconectar_BD(db):
+    db.close()
+    
---
+++ principal.py
@@ -0,0 +1,4 @@
+from funciones import *
+
+db = Conectar_BD("localhost","usuario","asdasd","tesdb")
+Desconectar_BD(db)
