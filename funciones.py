ca24fe6a83bbf025cace4ecfee6de4cc5ddfa5a6
José Domingo Muñoz <josedom24@gmail.com>
2022-02-08 18:20:45 +0100

    python

--- funciones.py
+++ funciones.py
@@ -3,12 +3,11 @@ import MySQLdb
 
 def Conectar_BD(host,usuario,password,nombrebd):
     try:
-    	db = MySQLdb.connect(host,usuario,password,nombrebd)
+        db = MySQLdb.connect(host,usuario,password,nombrebd)
         return db
     except MySQLdb.Error as e:
-    	print("No puedo conectar a la base de datos:",e)
-    	sys.exit(1)
+        print("No puedo conectar a la base de datos:",e)
+        sys.exit(1)
 
 def Desconectar_BD(db):
     db.close()
-    
