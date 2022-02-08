faf213e1410549dfad686b18ba190589bd003345
José Domingo Muñoz <josedom24@gmail.com>
2022-02-08 18:31:00 +0100

    python

--- funciones.py
+++ funciones.py
@@ -11,3 +11,27 @@ def Conectar_BD(host,usuario,password,nombrebd):
 
 def Desconectar_BD(db):
     db.close()
+
+
+def Insertar_BD(db,alumno):
+    cursor = db.cursor()
+    sql="insert into Alumnos values ('%s', '%s', '%d', '%c', '%d' )" % (alumno["identificador"],alumno["nombre"],alumno["apellidos"],alumno["edad"])
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        print("Error al insertar.")
+        db.rollback()
+
+def MostrarMenu():
+    menu='''
+    1. Insertar Alumnos
+    0. Salir
+    '''
+    print(menu)
+    while True:
+        try:
+            opcion=int(input("Opción:"))
+            return opcion
+        except:
+            print("Opción incorrecta, debe ser un número")
--- principal.py
+++ principal.py
@@ -2,3 +2,13 @@ from funciones import *
 
 db = Conectar_BD("localhost","usuario","asdasd","testdb")
 Desconectar_BD(db)
+opcion=Mostrar_menu()
+while opcion!=0:
+    if opcion==1:
+        alumno={}
+        alumno["identificador"]=int(input("Identificador:"))
+        alumno["nombre"]=input("Nombre:")
+        alumno["apellidos"]=input("Apellidos:")
+        alumno["edad"]=int(input("Edad:"))
+        Insertar_BD(db,alumno)
+        
